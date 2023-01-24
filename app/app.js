b43d43921b7ec7bd2bbe98e0b49a2ccb51348ef9
Hansol YOO <78459995+yhansol145@users.noreply.github.com>
2023-01-24 20:03:22 +0900

    [node]  - dom을 이용한 객체 저장  - nodemon 적용  - 폴더구조 최적화

--- app/app.js
+++ app/app.js
@@ -6,11 +6,12 @@ const app = express();
 const PORT = 3000;
 
 // 라우팅
-const home = require("./routes/home");
+const home = require("./src/routes/home");
 
 // 앱 세팅
-app.set("views", "./views");
+app.set("views", "./src/views");
 app.set("view engine", "ejs");
+app.use(express.static(`${__dirname}/src/public`));
 
 app.use("/", home); // use -> 미들웨어를 등록해주는 메소드
 module.exports = app; // app 내보내기
---
+++ app/src/public/js/home/login.js
@@ -0,0 +1,17 @@
+"use strict";
+
+// DOM -> Document Object Model 자바스크립트에서 html데이터를 제어 할 수 있다.
+
+const id = document.querySelector("#id"),  //질의 선택자
+    psword = document.querySelector("#psword"),
+    loginBtn = document.querySelector("button");
+
+loginBtn.addEventListener("click", login);
+
+function login() {
+    const req = {
+        id: id.value,
+        psword: psword.value
+    };
+    console.log(req);
+}
